8ab643deb6cfddaae5ff2fe73f357899c00832fe
Your Name <bistjyoti64@gmail.com>
2026-05-25 23:02:46 +0530

    fix: installed react-speech-recognition dependency

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import { assets } from '../../assets/assets';
 import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../context/StoreContext';
+import 'regenerator-runtime/runtime';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Navbar = ({ setShowLogin, searchQuery, setSearchQuery }) => {
