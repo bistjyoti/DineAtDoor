767e5df7665d6e1f70d9f28ea254cfd79f48e5bd
Your Name <bistjyoti64@gmail.com>
2026-05-31 17:45:26 +0530

    Success: Backend switched to live Render URL

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,14 +7,18 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
+                {/* 🎯 FIXED: Yahan class add kar di hai taaki CSS isse control kar sake */}
+                <img src={assets.logo} className='footer-logo' alt="DineAtDoor Logo" />
+                
                 <p>DineAtDoor is your favorite food delivery partner, bringing fresh and delicious meals right to your doorstep with speed and care.</p>
+                
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <img src={assets.facebook_icon} alt="Facebook" />
+                    <img src={assets.twitter_icon} alt="Twitter" />
+                    <img src={assets.linkedin_icon} alt="Linkedin" />
                 </div>
             </div>
+
             <div className="footer-content-center">
                 <h2>COMPANY</h2>
                 <ul>
@@ -24,6 +28,7 @@ const Footer = () => {
                     <li>Privacy Policy</li>
                 </ul>
             </div>
+
             <div className="footer-content-right">
                 <h2>GET IN TOUCH</h2>
                 <ul>
@@ -35,10 +40,10 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer-copyright">
-            Copyright 2026 &copy; DineAtDoor- All Right Reserved.
+            Copyright 2026 &copy; DineAtDoor - All Rights Reserved.
         </p>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
